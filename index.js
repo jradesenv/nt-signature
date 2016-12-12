a1dc7fe7ebfe9b6e82dcccb152a2b732f55ffa50
jean robert <jean.alves@ntconsult.com.br>
2016-12-12 10:50:51 -0200

    campo telefone

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ var ViewModel = function() {
     _view.ultimoNome = ko.observable("");
     _view.cargo = ko.observable("");
     _view.email = ko.observable("");
+    _view.telefone = ko.observable("");
+    _view.telefoneTratado = ko.observable("");
+
+    _view.telefone.subscribe(function(newValue) {
+        var _telefone = _view.telefone();
+        if(_telefone.length > 0) {
+            _telefone = " / " + _telefone;
+        }
+        _view.telefoneTratado(_telefone);
+    });
 
     _view.abrirPopup = function() {
         var OpenWindow=window.open("url", "newwin", "height=500, width=500,toolbar=no,scrollbars=yes,menubar=no");
@@ -16,10 +26,35 @@ var ViewModel = function() {
         OpenWindow.document.write(_htmlPopup);
     }
 };
+
+ko.bindingHandlers.masked = {
+    init: function(element, valueAccessor, allBindingsAccessor) {
+        var mask = allBindingsAccessor().mask || {};
+        $(element).mask(mask);
+        ko.utils.registerEventHandler(element, 'focusout', function() {
+            var observable = valueAccessor();
+            observable($(element).val());
+        });
+    }, 
+    update: function (element, valueAccessor) {
+        var value = ko.utils.unwrapObservable(valueAccessor());
+        $(element).val(value);
+    }
+};
  
 ko.applyBindings(new ViewModel());
 
-// var txtResultado = document.getElementById("txtResultado");
-// txtResultado.onfocus = function() {
-//     txtResultado.select();
-// };
+$("input.inputTelefone")
+        .mask("(999) 9999-9999?9")
+        .focusout(function (event) {  
+            var target, phone, element;  
+            target = (event.currentTarget) ? event.currentTarget : event.srcElement;  
+            phone = target.value.replace(/\D/g, '');
+            element = $(target);  
+            element.unmask();  
+            if(phone.length > 11) {  
+                element.mask("(999) 99999-999?9");  
+            } else {  
+                element.mask("(999) 9999-9999?9");  
+            }  
+        });
