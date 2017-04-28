9651975dff17d5e62a0340dad696e80dd5b861e6
Jean Robert <jradesenv@gmail.com>
2017-04-28 16:34:36 -0300

    usando variavel de ambiente para deploy no heroku

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(3520, function(){
-    console.log('Server running on 3000...');
+var port = process.env.PORT || 3000;
+
+connect().use(serveStatic(__dirname)).listen(port, function(){
+    console.log('Server running on ' + port + '...');
 });
